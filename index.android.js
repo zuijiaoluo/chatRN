921e95dbf30ad5a1d82c71ed6d872eb5f1368d83
zuijiaoluo <zuijiaoluo@outlook.com>
2016-10-10 10:41:23 +0800

    test

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ class chatRN extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          欢迎使用 chatRN!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
